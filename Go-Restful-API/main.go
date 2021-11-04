7796d5197f6f7f41ce687b73bdf55abb909ce2fd
quangcuong <cophongsuongbn@gmail.com>
2021-11-04 16:59:33 +0700

    add source

--- Go-Restful-API/main.go
+++ Go-Restful-API/main.go
@@ -1,4 +1,5 @@
 //kết nối mysql từ docker sang go lang -> Ko phải cài mysql trên local
+// https://www.youtube.com/watch?v=6-x1CYP5Axo&ab_channel=EricLau
 package main
 
 import "go-restful-api/api"
